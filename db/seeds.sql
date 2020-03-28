bf43997b86ca6e5f94ff06b722dae6a872649893
Hector Agudelo <haacny86@gmail.com>
2020-03-27 20:14:03 -0400

    created partial views, also the controller, the connections, and the server is done

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (name,devoured) VALUES ('cheeseburger',false);
+INSERT INTO burgers (name, devoured) VALUES ('vegieburger',false);
+INSERT INTO burgers (name, devoured) VALUES ('chickenburger',false);
