56e1ee1395055ac147f2bf1314c72bdf4d5ccac0
Hector Agudelo <haacny86@gmail.com>
2020-03-30 21:21:05 -0400

    modifications to config files to be able to uploaded to heroku

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,12 @@
 var mysql = require("mysql");
+const database = require('./keys');
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "hectoragudelo",
-  password: "12345678",
-  database: "burgers_db"
-});
-
+if (process.env.JAWSDB.URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+}
+else {
+  var connection = mysql.createConnection(database);
+}
 // Make connection.
 connection.connect(function (err) {
   if (err) {
@@ -16,6 +15,7 @@ connection.connect(function (err) {
   }
   console.log("connected as id " + connection.threadId);
 });
+console.log(database)
 
 // Export connection for our ORM to use.
 module.exports = connection;
