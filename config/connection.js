10e16d26228df1ca4fbe9298ad65843265d2b79b
Hector Agudelo <haacny86@gmail.com>
2020-03-30 22:43:04 -0400

    better2

--- config/connection.js
+++ config/connection.js
@@ -24,7 +24,7 @@ connection.connect(function (err) {
   }
   console.log("connected as id " + connection.threadId);
 });
-console.log(database)
+
 
 // Export connection for our ORM to use.
 module.exports = connection;
--- config/orm.js
+++ config/orm.js
@@ -51,7 +51,7 @@ var orm = {
     queryString += ") ";
 // insert into tabla(burgers) ((col.tostring()) name, devour);
 //values (name, devour);
-    console.log(queryString);
+    
 
     connection.query(queryString, vals, function(err, result) {
       if (err) {
