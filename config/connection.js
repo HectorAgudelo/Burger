6e539a41d085adf04ad19067199c21fd7a6db7ac
Hector Agudelo <haacny86@gmail.com>
2020-03-30 22:32:57 -0400

    better

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,17 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "hectoragudelo",
-  password: "12345678",
-  database: "burgers_db"
-});
-
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+}
+else {
+  var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "hectoragudelo",
+    password: "12345678",
+    database: "burgers_db"
+  });
+}
 // Make connection.
 connection.connect(function (err) {
   if (err) {
